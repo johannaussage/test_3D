55fe6046c1e70415fd0c9522f153c0d8abee5634
johannaussage <noreply>
2014-09-28 13:01:24 +0000

    Add component 3D_assets

---
+++ ui/3-d_assets.reel/3-d_assets.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/3-d_assets.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class 3D_assets
+ * @extends Component
+ */
+exports.3D_assets = Component.specialize(/** @lends 3D_assets# */ {
+    constructor: {
+        value: function 3D_assets() {
+            this.super();
+        }
+    }
+});
