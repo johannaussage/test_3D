dc385c01011adbc9aef20908b3658d90c8b566b9
johannaussage <noreply>
2014-09-28 13:26:17 +0000

    Add component npm install foo

---
+++ ui/npm-install-foo.reel/npm-install-foo.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/npm-install-foo.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class NpmInstallFoo
+ * @extends Component
+ */
+exports.NpmInstallFoo = Component.specialize(/** @lends NpmInstallFoo# */ {
+    constructor: {
+        value: function NpmInstallFoo() {
+            this.super();
+        }
+    }
+});
